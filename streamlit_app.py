918634de448ab0d10d7f3989bc1a23203702e06e
Kek Yi Ci <22102539@siswa.um.edu.my>
2024-05-31 05:41:11 +0000

    kyc

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,8 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 with st.sidebar:
     selected = option_menu(
-        menu_title = "S.M.A.R.T",
-        options = ["Home", "Data Exploration", "Analysis", "Predictions", "Model", "Download CSV", "Settings", "Contributors"],
+        menu_title = "Census Income Prediction",
+        options = ["Home", "Analysis", "Predictions", "Model"],
         icons = ["house", "upload", "graph-up", "magic", "speedometer", "file-earmark-arrow-down", "gear", "people"],
         menu_icon= "robot",
         default_index = 0,
@@ -60,59 +60,34 @@ with st.sidebar:
 
 # ========= HOME TAB =========
 if selected == "Home":
-    col1, col2 = st.columns([2, 1])
-    with col1:
-        st.title('S.M.A.R.T :rocket:')
-        st.header("Student Management 🏠 And")
-        st.header("Recruitment Tool 🔮")
-        st.divider()
-        st.header("About :memo:")
-        st.markdown('''
-        ####
-        We are thrilled to introduce you to SMART, your all-in-one solution for student
-        management and recruitment needs
-        providing a comprehensive platform for tracking academic progress, 
-        facilitating career development, and predicting placement opportunities.
-        With SMART, educational institutions can efficiently manage student data,
-        track their academic performance, and streamline the recruitment process. 
-        Meanwhile, students can access personalized career guidance, 
-        explore job opportunities, and receive tailored recommendations
-        for enhancing their employability. 
-        Join us on this exciting journey as we revolutionize
-        student management and recruitment with SMART!
-        ''')
-        
-        st.markdown("#### `Get Started Now!`")
+    st.title('Census Income Prediction')
+    st.divider()
+    st.header("About :memo:")
+    st.markdown('''
+    ####
+    We are thrilled to introduce you to Income Predictor, 
+    your all-in-one solution for predicting income levels 
+    based on census data. Our platform provides a 
+    comprehensive tool for analyzing demographic and 
+    employment information to predict income levels with 
+    high accuracy.
+                
+    With Income Predictor, organizations and researchers 
+    can efficiently manage and analyze large datasets, 
+    track various demographic metrics, and make informed 
+    predictions about income distribution. Our platform 
+    leverages advanced machine learning algorithms to 
+    deliver precise income predictions, helping you 
+    understand socioeconomic trends and make data-driven 
+    decisions.
 
-    with col2:
-        st.image("Images\\home_tab_pic.png")
+    Join us on this exciting journey as we revolutionize 
+    income prediction and demographic analysis with Income 
+    Predictor!
+    ''')
+    
+    st.markdown("#### `Get Started Now!`")
 
-# ========= UPLOAD TAB =========
-if selected == "Data Exploration":
-
-    st.title("Census Income Prediction Web App")
-    st.header("Explore the dataset and predict income levels.")
-    # Data Exploration Page
-    # Sidebar for data exploration options
-
-    st.header("Data Exploration ")
-    st.subheader("Summary Statistics")
-    st.write(data.describe())
-
-    st.subheader("Data Visualization")
-    selected_feature = st.selectbox("Select Feature for Visualization", data.columns)
-    selected_chart = st.selectbox("Select Chart Type", ["Histogram", "Bar Chart", "Scatter Plot"])
-    filtered_data = data[data["income"] == 1] if st.checkbox("Filter High Income") else data
-
-
-
-    # Data visualization
-    if selected_chart == "Histogram":
-        st.bar_chart(filtered_data[selected_feature].value_counts())
-    elif selected_chart == "Bar Chart":
-        st.bar_chart(filtered_data[selected_feature].value_counts())
-    else:
-        st.scatter_chart(filtered_data.sample(100), x="age", y="hours_per_week")
 
 
 # ========= ANALYSIS TAB =========
@@ -120,12 +95,8 @@ if selected == "Analysis":
    
         st.balloons()
         st.title("Data Analysis 📊")
-        st.title("Census Income Prediction Web App")
         st.header("Explore the dataset and predict income levels.")
-        # Data Exploration Page
-        # Sidebar for data exploration options
-
-        st.header("Data Exploration ")
+    
         st.subheader("Summary Statistics")
         st.write(data.describe())
 
@@ -145,7 +116,7 @@ if selected == "Analysis":
             st.scatter_chart(filtered_data.sample(100), x="age", y="hours_per_week")
 
 # ========= PREDICTION TAB =======
-def transform_input(age, workclass, fnlwgt, education, education_num, marital_status, occupation, relationship, race, sex, capital_gain, capital_loss, hours_per_week, native_country):
+def transform_input(age, workclass, education, marital_status, occupation, relationship, sex, hours_per_week):
     # Define transformation mappings
     workclass_mapping = {
         'Private': 0, 'Self-emp-not-inc': 1, 'Self-emp-inc': 2, 'Federal-gov': 3, 'Local-gov': 4,
@@ -165,21 +136,11 @@ def transform_input(age, workclass, fnlwgt, education, education_num, marital_st
     relationship_mapping = {
         'Married': 0, 'Single': 1, 'Separated': 2
     }
-    race_mapping = {
-        'White': 0, 'Asian-Pac-Islander': 1, 'Amer-Indian-Eskimo': 2, 'Other': 3, 'Black': 4
-    }
+
     sex_mapping = {
         'Male': 0, 'Female': 1
     }
-    native_country_mapping = {
-        'United-States': 0, 'Cambodia': 1, 'England': 2, 'Puerto-Rico': 3, 'Canada': 4, 'Germany': 5,
-        'Outlying-US(Guam-USVI-etc)': 6, 'India': 7, 'Japan': 8, 'Greece': 9, 'South': 10, 'China': 11,
-        'Cuba': 12, 'Iran': 13, 'Honduras': 14, 'Philippines': 15, 'Italy': 16, 'Poland': 17, 'Jamaica': 18,
-        'Vietnam': 19, 'Mexico': 20, 'Portugal': 21, 'Ireland': 22, 'France': 23, 'Dominican-Republic': 24,
-        'Laos': 25, 'Ecuador': 26, 'Taiwan': 27, 'Haiti': 28, 'Columbia': 29, 'Hungary': 30, 'Guatemala': 31,
-        'Nicaragua': 32, 'Scotland': 33, 'Thailand': 34, 'Yugoslavia': 35, 'El-Salvador': 36, 'Trinadad&Tobago': 37,
-        'Peru': 38, 'Hong': 39, 'Holand-Netherlands': 40
-    }
+
 
     # Apply transformations
     workclass_encoded = workclass_mapping.get(workclass, -1)
@@ -187,38 +148,27 @@ def transform_input(age, workclass, fnlwgt, education, education_num, marital_st
     marital_status_encoded = marital_status_mapping.get(marital_status, -1)
     occupation_encoded = occupation_mapping.get(occupation, -1)
     relationship_encoded = relationship_mapping.get(relationship, -1)
-    race_encoded = race_mapping.get(race, -1)
     sex_encoded = sex_mapping.get(sex, -1)
-    native_country_encoded = native_country_mapping.get(native_country, -1)
-
-    return [age, workclass_encoded, fnlwgt, education_encoded, education_num, marital_status_encoded, occupation_encoded, relationship_encoded, race_encoded, sex_encoded, capital_gain, capital_loss, hours_per_week, native_country_encoded]
+    return [age, workclass_encoded, education_encoded, marital_status_encoded, occupation_encoded, relationship_encoded, sex_encoded,hours_per_week]
 
 if selected == "Predictions":
-    col1, col2 = st.columns(2)
-    with col1:
-        st.title("Income Level Prediction ⚡")
-        st.subheader("Provide the inputs below 👇🏻")
-        st.divider()
-        st.markdown("##### _Here we will use <span style='color:yellow'>Random Forest 🤖</span> Machine Learning Algorithm to create our Model to predict the Income Level of Individuals_.", unsafe_allow_html=True)
-        st.markdown("##### _You just need to provide the individual's data to get started and predict their income level using our <span style='color:yellow'>well trained Model right here</span>_.", unsafe_allow_html=True)
     
+    st.title("Income Level Prediction ⚡")
+    st.subheader("Provide the inputs below 👇🏻")
+    st.divider()
+    st.markdown("##### _Here we will use <span style='color:yellow'>Random Forest 🤖</span> Machine Learning Algorithm to create our Model to predict the Income Level of Individuals_.", unsafe_allow_html=True)
+    st.markdown("##### _You just need to provide the individual's data to get started and predict their income level using our <span style='color:yellow'>well trained Model right here</span>_.", unsafe_allow_html=True)
+
     col1, col2 = st.columns(2)
     with col1:
         # Get user input for Age
         age = st.slider('Enter the Age 👇🏻', min_value=18, max_value=90, step=1)
-        # Get user input for fnlwgt
-        fnlwgt = st.number_input('Enter the fnlwgt 👇🏻', min_value=0)
-        # Get user input for education_num
-        education_num = st.slider('Enter the Education Num 👇🏻', min_value=1, max_value=16, step=1)
-        # Get user input for capital_gain
-        capital_gain = st.number_input('Enter the Capital Gain 👇🏻', min_value=0)
-        # Get user input for capital_loss
-        capital_loss = st.number_input('Enter the Capital Loss 👇🏻', min_value=0)
-        # Get user input for hours_per_week
+       # Get user input for hours_per_week
         hours_per_week = st.slider('Enter the Hours Per Week 👇🏻', min_value=1, max_value=100, step=1)
+        occupation = st.selectbox('Choose Occupation 💼', ['Blue_collar', 'White_collar', 'Brown_collar/Protective_service', 'Pink_collar/Service_and_sales'])
+
+        model_choice = st.selectbox("Select Model", ["Random Forest", "Decision Tree", "Naive Bayes", "Logistic Regression"])
         predict_button = st.button("Predict the Income Level ⚡")
-        model_choice = st.selectbox("Select Model", ["Random Forest", "Decision Tree", "SVM", "Naive Bayes", "Logistic Regression"])
-    
 
     with col2:
         # Get user input for Workclass
@@ -227,34 +177,29 @@ if selected == "Predictions":
         education = st.selectbox('Choose Education Level 🎓', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
         # Get user input for Marital Status
         marital_status = st.selectbox('Choose Marital Status 💍', ['Single', 'Married'])
-        # Get user input for Occupation
-        occupation = st.selectbox('Choose Occupation 💼', ['Blue_collar', 'White_collar', 'Brown_collar/Protective_service', 'Pink_collar/Service_and_sales'])
-        # Get user input for Relationship
+
         relationship = st.selectbox('Choose Relationship Status 💑', ['Married', 'Single', 'Separated'])
-        # Get user input for Race
-        race = st.selectbox('Choose Race 🌍', ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
         # Get user input for Sex
         sex = st.selectbox('Choose Sex 🧑🏻‍🦱👧🏻', ['Male', 'Female'])
         # Get user input for Native Country
-        native_country = st.selectbox('Choose Native Country 🌎', ['United-States', 'Cambodia', 'England', 'Puerto-Rico', 'Canada', 'Germany', 'Outlying-US(Guam-USVI-etc)', 'India', 'Japan', 'Greece', 'South', 'China', 'Cuba', 'Iran', 'Honduras', 'Philippines', 'Italy', 'Poland', 'Jamaica', 'Vietnam', 'Mexico', 'Portugal', 'Ireland', 'France', 'Dominican-Republic', 'Laos', 'Ecuador', 'Taiwan', 'Haiti', 'Columbia', 'Hungary', 'Guatemala', 'Nicaragua', 'Scotland', 'Thailand', 'Yugoslavia', 'El-Salvador', 'Trinadad&Tobago', 'Peru', 'Hong', 'Holand-Netherlands'])
-
+       
     # Check if the Predict Income button is clicked
     if predict_button:
         st.balloons()
         
         # Transform the input features
-        user_data = transform_input(age, workclass, fnlwgt, education, education_num, marital_status, occupation, relationship, race, sex, capital_gain, capital_loss, hours_per_week, native_country)
+        user_data = transform_input(age, workclass, education, marital_status, occupation, relationship, sex, hours_per_week)
      # Prepare the user input as a dataframe
         user_df = pd.DataFrame([user_data], columns=[
-            'age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status',
-            'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country'
+            'age', 'workclass', 'education', 'marital_status',
+            'occupation', 'relationship', 'sex',  'hours_per_week'
         ])
         
         st.divider()
         st.markdown("* ## Input Dataframe ⬇️")
         st.write(user_df)
-
-        X = data.drop("income", axis=1)
+       
+        X = data.drop(['education_num','fnlwgt','capital_gain','capital_loss','native_country','race','income'], axis=1)
         y = data["income"]
 
         # Label Encoding
@@ -271,8 +216,6 @@ if selected == "Predictions":
             clf = RandomForestClassifier()
         elif model_choice == "Decision Tree":
             clf = DecisionTreeClassifier()
-        elif model_choice == "SVM":
-            clf = SVC()
         elif model_choice == "Naive Bayes":
             clf = GaussianNB()
         elif model_choice == "Logistic Regression":
